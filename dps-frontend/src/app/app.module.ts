bbaf0ad01bbb4d55dc667f9fe8e88dce1974bd29
Dan-Baba <dan.foote104227@gmail.com>
2018-01-27 09:11:34 -0500

    Setup dev and prod environment.

--- dps-frontend/src/app/app.module.ts
+++ dps-frontend/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, enableProdMode } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
 
+
+if (environment.production) {
+  enableProdMode();
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ...environment.imports
   ],
   providers: [],
   bootstrap: [AppComponent]
