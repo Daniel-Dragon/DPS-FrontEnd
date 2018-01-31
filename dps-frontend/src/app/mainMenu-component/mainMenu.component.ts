f05772bd1fd4a17cdfe17eb24ac2569072c4b9e2
mparrella823 <qaj65ut%>
2018-01-31 09:27:30 -0500

    fixing (dumb) syntax errors

--- dps-frontend/src/app/mainMenu-component/mainMenu.component.ts
+++ dps-frontend/src/app/mainMenu-component/mainMenu.component.ts
@@ -1,9 +1,10 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'main-menu',
-    templateUrl: './mainMenu.component.html'
-})
+    selector: 'app-main-menu',
+    templateUrl: './mainMenu.component.html',
+    styleUrls: ['./mainMenu.component.css']
+    })
 export class MainMenuComponent {
     collapsed = true;
 }
